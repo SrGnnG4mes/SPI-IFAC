19e70e0366ff01e806a3341f1b5a8e92a8980e6d
Gabriel <gabrielnogueira.ac@gmail.com>
2026-03-18 14:07:07 -0500

    Atividade dia 11/03

--- 5- périodo/PaginasWeb-II/Aula0403/src/banco/ContaEmpresarial.java
+++ 5- périodo/PaginasWeb-II/Aula0403/src/banco/ContaEmpresarial.java
@@ -1,25 +1,11 @@
 package banco;
 
 public class ContaEmpresarial extends Conta{
-    public double limite;
 
-    public ContaEmpresarial(int numero, String dono, double limite) {
-        super(numero, dono);
+    public ContaEmpresarial(int numero,String dono){
+        super(numero,dono);
         this.limite = 1200;
     }
 
-    @Override
-    public void sacar(double valor) {
-        super.sacar(valor);
-    }
 
-    @Override
-    public void depositar(double valor) {
-        super.depositar(valor);
-    }
-
-    @Override
-    public double verSaldo() {
-        return super.verSaldo();
-    }
 }
