f12e446e7a3cfdce06850e2c925281710180ddce
Gabriel <gabrielnogueira.ac@gmail.com>
2026-03-25 15:35:31 -0500

    Começando Springboot

---
+++ 5- périodo/PaginasWeb-II/Springboot/demo/src/main/java/com/example/demo/controladores/AlunoControlador.java
@@ -0,0 +1,44 @@
+package com.example.demo.controladores;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLOutput;
+
+@RestController
+public class AlunoControlador {
+
+    @GetMapping("/oiMundo")
+    public String oiMundo(){
+        return "Olá Mundo!";
+    }
+
+    @GetMapping("/somar")
+    public int somar(@RequestParam("num1")int num1,@RequestParam("num2")int num2){
+
+        return num1 + num2;
+    }
+
+    @GetMapping("/somar/{num1}/{num2}")
+    public int somar2(@PathVariable("num1")int num1, @PathVariable("num2")int num2){
+        return num1 + num2;
+    }
+
+    @GetMapping("/subtrair/{num1}/{num2}")
+    public int subtrair(@PathVariable("num1")int num1, @PathVariable("num2")int num2){
+        return num1 - num2;
+    }
+
+    @GetMapping("/multiplicar/{num1}/{num2}")
+    public int multiplicar(@PathVariable("num1")int num1, @PathVariable("num2")int num2){
+        return num1 * num2;
+    }
+
+    @GetMapping("/dividir/{num1}/{num2}")
+    public int dividir(@PathVariable("num1")int num1, @PathVariable("num2")int num2){
+        return num1 / num2;
+    }
+
+}
