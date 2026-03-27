57469b0236a56441935e8245b36a548f5c0c205b
Gabriel <gabrielnogueira.ac@gmail.com>
2026-03-27 16:15:50 -0500

    Adicionando dependencias

--- 5- périodo/PaginasWeb-II/Springboot/demo/src/main/java/com/example/demo/modelos/Aluno.java
+++ 5- périodo/PaginasWeb-II/Springboot/demo/src/main/java/com/example/demo/modelos/Aluno.java
@@ -1,6 +1,14 @@
 package com.example.demo.modelos;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Aluno {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
     private String nome;
     private String telefone;
