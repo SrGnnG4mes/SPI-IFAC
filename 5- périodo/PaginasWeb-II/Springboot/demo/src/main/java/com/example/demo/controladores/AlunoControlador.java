569dfea92d19b4111a38e67d13e16553ac023df8
Gabriel <gabrielnogueira.ac@gmail.com>
2026-04-08 15:51:15 -0500

    Aula0804

--- 5- périodo/PaginasWeb-II/Springboot/demo/src/main/java/com/example/demo/controladores/AlunoControlador.java
+++ 5- périodo/PaginasWeb-II/Springboot/demo/src/main/java/com/example/demo/controladores/AlunoControlador.java
@@ -9,14 +9,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLOutput;
+import java.util.List;
 
 @RestController
+@RequestMapping("/aluno")
 public class AlunoControlador {
 
     @Autowired
     AlunoServico alunoServico;
 
-    @PostMapping("/aluno/salvar")
+    @PostMapping("/salvar")
     public ResponseEntity<Aluno> salvar(@RequestBody Aluno aluno){
 
         alunoServico.salvar(aluno);
@@ -26,6 +28,38 @@ public class AlunoControlador {
         return new ResponseEntity<Aluno>(aluno, HttpStatus.OK);
 
         //return "Oi " + aluno.getNome() + "\n" + aluno.getTelefone() + "\n" + aluno.getIdade();
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<Aluno>> buscar(){
+        List<Aluno> alunos = alunoServico.buscarTodos();
+        return new ResponseEntity<List<Aluno>>(alunos, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Aluno> buscarPeloId(@PathVariable Long id){
+        try {
+            Aluno aluno = alunoServico.buscarPeloId(id);
+            return new ResponseEntity<Aluno>(aluno, HttpStatus.OK);
+        }catch (IllegalArgumentException erro){
+            return new ResponseEntity<Aluno>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Aluno> excluir(@PathVariable Long id){
+        try {
+            alunoServico.excluir(id);
+        }catch (IllegalArgumentException erro){
+            return new ResponseEntity<Aluno>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<Aluno>(HttpStatus.OK);
+    }
+
+    @PutMapping("")
+    public ResponseEntity<Aluno> editar(@RequestBody Aluno aluno){
+        alunoServico.editar(aluno);
+        return new ResponseEntity<Aluno>(HttpStatus.OK);
     }
 
 }
--- 5- périodo/PaginasWeb-II/Springboot/demo/src/main/java/com/example/demo/servicos/AlunoServico.java
+++ 5- périodo/PaginasWeb-II/Springboot/demo/src/main/java/com/example/demo/servicos/AlunoServico.java
@@ -2,8 +2,14 @@ package com.example.demo.servicos;
 
 import com.example.demo.modelos.Aluno;
 import com.example.demo.repositorios.AlunoRepositorio;
+import org.hibernate.boot.models.annotations.spi.AttributeMarker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AlunoServico {
@@ -12,6 +18,36 @@ public class AlunoServico {
     AlunoRepositorio alunoRepositorio;
 
     public void salvar(Aluno aluno){
+        alunoRepositorio.save(aluno);
+    }
+    public List<Aluno> buscarTodos(){
+        return alunoRepositorio.findAll();
+    }
+
+    public Aluno buscarPeloId(@PathVariable Long id){
+
+        Optional<Aluno> optionalAluno = alunoRepositorio.findById(id);
+
+        if (optionalAluno.isEmpty()){
+            throw new IllegalArgumentException("Aluno não foi encontrado");
+        }else {
+            return optionalAluno.get();
+        }
+
+    }
+
+    public void excluir(Long id){
+
+        Optional<Aluno> optionalAluno = alunoRepositorio.findById(id);
+
+        if (optionalAluno.isEmpty()){
+            throw new IllegalArgumentException("Aluno não foi encontrado");
+        }else {
+            alunoRepositorio.deleteById(id);
+        }
+    }
+
+    public void editar(Aluno aluno){
         alunoRepositorio.save(aluno);
     }
 }
